75d08b51a38d002f44fa8f7918ab8b3069d298c2
brettg17 <brettgelinas7@gmail.com>
2024-01-24 14:53:46 -0600

    updated to have template literals

--- assertEqual.js
+++ assertEqual.js
@@ -3,9 +3,9 @@
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    console.log("✅✅✅Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log("🛑🛑🛑Assertion failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
   }
   return actual + expected;
 
