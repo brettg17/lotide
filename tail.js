5706a27b4bf8af8bce6d1f391eb7a84c3981ab01
brettg17 <brettgelinas7@gmail.com>
2024-01-25 17:41:04 -0600

    created function 'tail': returns all elements of array except first

---
+++ tail.js
@@ -0,0 +1,15 @@
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+  }
+  return actual + expected;
+
+};
+
+//TEST CODE
+console.assert(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.assert(assertEqual(1, 1));
