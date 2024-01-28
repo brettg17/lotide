783b154b5ad7f3b55efb15a797f2996096126d6c
brettg17 <brettgelinas7@gmail.com>
2024-01-28 10:01:33 -0600

    copied eqArrays function to assertArraysEqual.js file

---
+++ assertArraysEqual.js
@@ -0,0 +1,17 @@
+// check if elements are same length. return false if not/
+const eqArrays = function (arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+    }
+
+  //iteratre through each character of an array. return false if each element is not identical.
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+// else: returns true
+  return true;
+}
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [1, 2, 5, 3])); // => false
