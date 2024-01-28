98723be9834ccbff703fbd52cfd0d71696c47773
brettg17 <brettgelinas7@gmail.com>
2024-01-28 08:45:51 -0600

    created to do list for eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,7 @@
+/*
+To do:
+- copy source fil and paste to the top of eqArrays.js.
+- implement fucntion eqArrays.js. 
+  - takes twoa rrays and returns true or false
+  - use assertEqual to write test cases for various scenarios
+*/
