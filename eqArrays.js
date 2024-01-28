1a90e71895f39da9b41111a89b92c3857537e055
brettg17 <brettgelinas7@gmail.com>
2024-01-28 08:47:44 -0600

    added assertEqual file to eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -5,3 +5,19 @@ To do:
   - takes twoa rrays and returns true or false
   - use assertEqual to write test cases for various scenarios
 */
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+  }
+  return actual + expected;
+
+};
+
+//TEST CODE
+console.assert(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.assert(assertEqual(1, 1));
+
