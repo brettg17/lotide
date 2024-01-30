cd15bbfa119934bbebb346970182f950ee1a69d1
brettg17 <brettgelinas7@gmail.com>
2024-01-30 10:48:17 -0600

    test case passes 5 times before throwing error

--- letterPositions.js
+++ letterPositions.js
@@ -14,13 +14,58 @@ to do:
 const letterPositions = function(sentence) {
   const results = {};
   // logic to upate results here
+  for (let i = 0; i < sentence.length; i++) {
+    let total = sentence[i];
+    if (total !== ' ') {
+      if(!results[total]) {
+        results[total] = [];
+      }
+
+      results[total].push(i)
+    }
+    
+  }
 
   return results;
 
 }
 
 
-const obj = {
-  letter: []
+const eqArrays = function (arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+    }
+
+  //iteratre through each character of an array. return false if each element is not identical.
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+// else: returns true
+  return true;
 }
 
+
+//uses eqArrays function to iterate through array and then takes own arguments and displays
+//whether assertion has passed or failed.
+const assertArraysEqual = function (actual, expected) {
+  if(eqArrays(actual, expected)){
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+
+  }
+
+}
+const obj = letterPositions("lighthouse in the house");
+assertArraysEqual(obj["l"], [0])
+assertArraysEqual(obj["i"], [1, 11])
+assertArraysEqual(obj["g"], [2])
+assertArraysEqual(obj["h"], [3, 5, 15, 18])
+assertArraysEqual(obj["t"], [4, 14])
+assertArraysEqual(obj["0"], [6, 19])
+assertArraysEqual(obj["u"], [7, 20])
+assertArraysEqual(obj["s"], [8, 21])
+assertArraysEqual(obj["e"], [9, 16, 22])
+assertArraysEqual(obj["n"], [12])
