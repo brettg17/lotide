d4562cb4f90879a1721571009ffeaf2f0bd8c775
brettg17 <brettgelinas7@gmail.com>
2024-01-30 08:55:36 -0600

    wrote a to do section

---
+++ letterPositions.js
@@ -0,0 +1,13 @@
+/*
+to do:
+- implement function called letterPositions
+- step 1: define objecct structure to return
+- step 2: write a test
+  - copy in assertArraysEqual and eqArrays function into file
+  - write a test with a small string ex: hello
+  - use assertArraysEqual to compare the 4 letters and their expected positions
+- step 3: implement the function
+  - implement the logic of the function
+- push work and mark activity compelete
+*/
+
