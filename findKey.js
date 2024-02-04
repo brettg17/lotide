f724a9d4c1ad3e5eabab22fb4b27359cd9bf7e88
brettg17 <brettgelinas7@gmail.com>
2024-02-04 10:45:37 -0600

    created started template defining the function to find key as well as an object where key is to be found. commented out final arrow function and console.log

--- findKey.js
+++ findKey.js
@@ -7,4 +7,21 @@ const assertEqual = function(actual, expected) {
   }
 
 };
+const findKey = function (object, callback) {
+
+}
+
+
+Object1 ({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+});
+
+// const results = assertEqual(x => x.stars === 2)); // => "noma"
+//console.log(results);
+
 
