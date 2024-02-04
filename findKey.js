1209c1b3893f8f3d54657d894846eb44602539b9
brettg17 <brettgelinas7@gmail.com>
2024-02-04 12:50:47 -0600

    finished findKey function with test cases. added comments for redability.

--- findKey.js
+++ findKey.js
@@ -7,12 +7,20 @@ const assertEqual = function(actual, expected) {
   }
 
 };
-const findKey = function (object, callback) {
+// take obj and callback as arguments. scans object and returns key when 
+//truthy condition is met. if no key is found. return undefined.
+const findKey = function (obj, callback) {
+  for (const key in obj) { //iterate through object
+    if (callback(obj[key])){ // calls callback with obj[key] argument, checks truthy.
+      return key; //return the object key if condition is met.
+    }
+  }
+  return undefined// return undefined if condition is not met.
 
-}
+};
 
 
-Object1 ({
+object1 = ({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
@@ -21,7 +29,15 @@ Object1 ({
   "Akelarre":  { stars: 3 }
 });
 
-// const results = assertEqual(x => x.stars === 2)); // => "noma"
-//console.log(results);
+const results = findKey(object1, x => x.stars === 2); // => "noma"
+console.log(results);
+
+const results2 = findKey(object1, x => x.stars > 5); // => undefined
+console.log(results2);
+
+assertEqual(results, "noma")
+assertEqual(results2, undefined)
+assertEqual(results, "nemo")
+assertEqual(results2, "Akaleri")
 
 
