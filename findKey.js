5910008060f5fa9704c36057267792005503e15c
brettg17 <brettgelinas7@gmail.com>
2024-02-04 10:19:04 -0600

    created findKey.js file

---
+++ findKey.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+  }
+
+};
+
