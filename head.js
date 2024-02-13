c0d3d35f8316b983ebf903a2e165e54c15cb7d7b
brettg17 <brettgelinas7@gmail.com>
2024-02-13 09:10:09 -0600

    refactored code. exported tester code to own head testing file.

--- head.js
+++ head.js
@@ -1,19 +1,5 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const head = function(arr) {
   return arr[0];
 };
 
-//TEST CODE
-assertEqual(head(["Lighthouse Labs", "Bootcamp"]), "Bootcamp");
-assertEqual(head([7, 6, 5, 6, 23]), 2);
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
