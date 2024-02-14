717b5aa689c0d8474b5db6a23d87af4875cedf71
brettg17 <brettgelinas7@gmail.com>
2024-02-14 13:20:24 -0600

    removed map as compass says it is not needed.

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const findKey = require('./findKey');
 const findKeyByValue= require('./findKeyByValue');
 const flatten= require('./flatten');
 const letterPositions= require('./letterPositions');
-const map = require('./map');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 
@@ -25,7 +24,6 @@ module.exports = {
   findKeyByValue: findKeyByValue,
   flatten: flatten,
   letterPositions: letterPositions,
-  map: map,
   takeUntil: takeUntil,
   without: without
 
