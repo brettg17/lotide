5d9a1a644bed94bd39969095fd3722ed92f082ca
brettg17 <brettgelinas7@gmail.com>
2024-02-14 10:02:17 -0600

    now using Mocha and Chai to test head.

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,23 @@
-const head = require('../head')
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
+const head = require('../head');
 
 
-//TEST CODE
-assertEqual(head(["Lighthouse Labs", "Bootcamp"]), "Bootcamp");
-assertEqual(head([7, 6, 5, 6, 23]), 2);
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe('#head', () => {
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns 'Bootcamp' for ['Lighthouse Labs', 'Bootcamp']", () => {
+    assert.strictEqual(head(["Lighthouse Labs", "Bootcamp"]), "Lighthouse Labs");
+  });
+  it("returns '5' for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+  it("returns 'Hello' for ['Hello, 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+
+});
