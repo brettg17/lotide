279a31e2ff02c2ac9747d2e49e808337b45af840
brettg17 <brettgelinas7@gmail.com>
2024-02-14 12:09:27 -0600

    created middle testing file that uses mocha and chai to test if middle elements of array are returned

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,17 @@
+const assert = require('chai').assert;
 const tail = require('../tail')
-const assertEqual = require('../assertEqual')
 
-//TEST CODE
-assertEqual(tail(["Lighthouse Labs", "Bootcamp"]).toString(), ["Bootcamp"].toString());
-assertEqual(tail([7, 6, 5, 6, 23]).toString(), [6, 5, 6, 23].toString());
-assertEqual(tail([5, 6, 7]).toString(), [6, 7].toString());
-assertEqual(tail([5, 6, 7]).toString(), [6].toString());
+
+describe('#tail', () =>{
+  it("returns 'Bootcamp' for ['Lighthouse Labs', 'Bootcamp']", () => {
+    assert.deepEqual(tail(['Lighthouse Labs', 'Bootcamp']), ['Bootcamp']);
+  });
+
+  it("returns '6, 5, 6, 23' for [7, 6, 5, 6, 23]", () => {
+    assert.deepEqual(tail(['Lighthouse Labs', 'Bootcamp']), ['Bootcamp']);
+  });
+});
+
+
 
 
