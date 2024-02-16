1deb821f6851cfcba2105e815e53ec0b8db5d3b1
brettg17 <brettgelinas7@gmail.com>
2024-02-16 10:14:38 -0600

    added comments for readability

--- countOnly.js
+++ countOnly.js
@@ -13,11 +13,11 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    if(itemsToCount[item]) {
-      if (results[item]) {
-        results[item] += 1;
+    if(itemsToCount[item]) { //check if current item exists in itemsToCount. 
+      if (results[item]) { // if the item exists. count it
+        results[item] += 1; //results + 1 if it exists 
       } else {
-       results[item] = 1;
+       results[item] = 1; // if not. set the count to 1
       }
     }
   }
@@ -45,7 +45,7 @@ assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
 assertEqual(result1["Agouhanna"], undefined);
 
-
+module.exports = countOnly;
 
 
 
