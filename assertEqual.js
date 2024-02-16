921d9b3c06362d31a8b10e3d9437040eef174930
brettg17 <brettgelinas7@gmail.com>
2024-02-16 09:30:44 -0600

    added comments for readability

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,9 @@
 
 const assertEqual = function(actual, expected) {
 
-  if (actual === expected) {
+  if (actual === expected) { // tests to see if a functions actual return is equal to what is expected. assertion passed if true
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
+  } else if (actual !== expected) {// tests to see if a functions actual return is not equal to what is expected. assertion fail if they are not equal
     console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
   }
  return actual + expected;
