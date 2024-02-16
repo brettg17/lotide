43e66f6e7eccffe37409c4dd34fbd671ab196d9f
brettg17 <brettgelinas7@gmail.com>
2024-02-16 10:18:41 -0600

    exported findKeyByValue function

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -38,3 +38,4 @@ assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That 70's Show"), undefined);
 
 
+module.exports = findKeyByValue;
